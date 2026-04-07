7eb35d768e9df74728550c7644d24bf3549a647b
Jonie6 <120833506+Jonie6@users.noreply.github.com>
2026-04-06 20:59:10 -0700

    Add files via upload

---
+++ CS220FinalProject/src/App.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String fileName;
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
